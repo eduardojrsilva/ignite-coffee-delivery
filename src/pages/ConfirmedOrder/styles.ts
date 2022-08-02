0d07c6b84edd5486a9a229a5413f70bb364fd724
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-02 14:30:08 -0300

    adding margin-top

--- src/pages/ConfirmedOrder/styles.ts
+++ src/pages/ConfirmedOrder/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
+  margin-top: ${convertPixelToRem(50)};
 `;
 
 export const Content = styled.div`
